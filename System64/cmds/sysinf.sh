45ffd3298e40ff80416af66ef6dd8e0a7eb64185
BV0073194 <61207595+BV0073194@users.noreply.github.com>
2022-03-23 18:21:53 -0700

    Add files via upload

---
+++ System64/cmds/sysinf.sh
@@ -0,0 +1,8 @@
+clear
+now=$(date) #+"%T")
+echo $'GCC 1.0.0-17-generic 
+\n#64-GCC' $now $'\n
+UTC 2022 x86_64 GNU/Linux Distro\nDeveloper: Bradley Verner'
+echo $'\n\nHit enter to return to shell ><\n'
+read
+bash main.sh
