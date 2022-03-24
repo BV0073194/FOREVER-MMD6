678e61ae80b13c51c09632344780dc8a2e9f5566
BV0073194 <61207595+BV0073194@users.noreply.github.com>
2022-03-23 19:29:01 -0700

    Add files via upload

--- System64/cmds/sysinf.sh
+++ System64/cmds/sysinf.sh
@@ -1,7 +1,7 @@
 clear
 now=$(date) #+"%T")
-echo $'GCC 1.0.0-17-generic 
-\n#64-GCC' $now $'\n
+echo $'FOREVER-MMD6 1.0.0-17-generic 
+\n#64-FOREVER-MMD6' $now $'\n
 UTC 2022 x86_64 GNU/Linux Distro\nDeveloper: Bradley Verner'
 echo $'\n\nHit enter to return to shell ><\n'
 read
