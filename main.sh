b9a7af147129d241c5b247fc8979be551eaf185c
BV0073194 <61207595+BV0073194@users.noreply.github.com>
2022-03-23 19:28:28 -0700

    Add files via upload

--- main.sh
+++ main.sh
@@ -1,14 +1,16 @@
 clear
 root_folder="FOREVER-MMD6"
 #-d ~/$root_folder/System64/boot ] 
-if [ -f ~/$root_folder/System64/boot/mmd6-boot-tests ]
+if [ -f ~/$root_folder/System64/boot/mmd6-boot-disk ]
 then
   echo -ne 'Hit {Enter} to boot MMD6-LINUX\n'
   read -p ''
   beep -f 5232 -l 2 -d 0 -r 0 -n
-  ##bash System64/gcc
+  bash System64/boot/mmd6-boot-disk 
 else
   echo -ne 'Hit {Enter} to enter MMD6-LINUX recovery mode\n'
   read -p ''
-  wget https://www.dropbox.com/sh/xiuioh21409nsj5j/AAkanlaoidsgmq90mdg/DCIM0001.png?raw=1
+  git -C ./RECOVERY-DRIVE clone https://github.com/BV0073194/FOREVER-MMD6 --quiet
+  clear
+  bash main.sh
 fi
