e3546a10cf395408f9a6597a89f86f25deae25de
BV0073194 <61207595+BV0073194@users.noreply.github.com>
2022-03-23 19:30:07 -0700

    Update sysinf.sh

--- System64/cmds/sysinf.sh
+++ System64/cmds/sysinf.sh
@@ -2,7 +2,7 @@ clear
 now=$(date) #+"%T")
 echo $'FOREVER-MMD6 1.0.0-17-generic 
 \n#64-FOREVER-MMD6' $now $'\n
-UTC 2022 x86_64 GNU/Linux Distro\nDeveloper: Bradley Verner'
+UTC 2022 x86_64 GNU/Linux Distro\nDeveloper: BV0073194\nGithub: https://github.com/BV0073194'
 echo $'\n\nHit enter to return to shell ><\n'
 read
 bash main.sh
