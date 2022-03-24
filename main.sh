cb74994b9440401f82d5636ab53a3e053e994a97
BV0073194 <61207595+BV0073194@users.noreply.github.com>
2022-03-23 19:59:03 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,16 +1,19 @@
+export NIXPKGS_ALLOW_UNFREE=1
+nix-env -iA nixpkgs.wineWowPackages.stable
 rm -rf RECOVERY-DRIVE
+read -p ''
 clear
 root_folder="FOREVER-MMD6"
 #-d ~/$root_folder/System64/boot ] 
 if [ -f ~/$root_folder/System64/boot/mmd6-boot-disk ]
 then
-  echo -ne 'Hit {Enter} to boot MMD6-LINUX\n'
+  echo -ne 'Hit {Enter} to boot MMD6-WINUX\n'
   read -p ''
   rm -rf RECOVERY-DRIVE
   bash System64/boot/mmd6-boot-disk 
 else
   mkdir -p RECOVERY-DRIVE
-  echo -ne 'Hit {Enter} to enter MMD6-LINUX recovery mode\n'
+  echo -ne 'Hit {Enter} to enter MMD6-WINUX recovery mode\n'
   read -p ''
   git -C ./RECOVERY-DRIVE clone https://github.com/BV0073194/FOREVER-MMD6 --quiet
   clear
